738bccd6c3b8fbb3e1ae60238bfb36119b6fe0a9
spuxx1701 <accounts@spuxx.dev>
2024-05-22 11:46:33 +0200

    chore: Allow providing name when creating character and use default name if none is provided

--- lib/character/create-character.ts
+++ lib/character/create-character.ts
@@ -4,9 +4,12 @@ import { Character } from './character.go';
 
 /**
  * Creates a new character.
+ * @param name The name of the character. Can be left blank to use the default name.
  */
-export const createNewCharacter = (): Character => {
+export const createNewCharacter = (name?: string): Character => {
   const character = new Character();
+  // Set name
+  character.general.name = name || 'Anonymous';
   // Add primary and secondary attributes
   character.primaryAttributes = primaryAttributes.map(
     (blueprint) => new PrimaryAttribute({ ...blueprint, owner: character }),
