904cf084e7689f0e123ed0d6736c883fc58272f0
spuxx1701 <accounts@spuxx.dev>
2024-05-23 17:41:35 +0200

    chore: Skills no longer include their category into their name

--- lib/game-objects/character/origin/origin.blueprint-mocks.ts
+++ lib/game-objects/character/origin/origin.blueprint-mocks.ts
@@ -7,22 +7,18 @@ export const characterOriginMocks: Blueprint<CharacterOrigin>[] = [
     suitableTraits: ['character.trait.academic-education', 'character.trait.arrogance'],
     primaryAttributeBonuses: { 'character.primary-attribute.cleverness': 1 },
     fixedSkillBonuses: {
-      'character.skill.crafting.computers': 2,
-      'character.skill.knowledge.general-knowledge': 3,
-      'character.skill.social.soft-skills': -1,
+      'character.skill.computers': 2,
+      'character.skill.general-knowledge': 3,
+      'character.skill.soft-skills': -1,
     },
     selectableSkillBonuses: [
       {
         value: 1,
-        skills: ['character.skill.vehicles.spaceships', 'character.skill.vehicles.airplanes'],
+        skills: ['character.skill.spaceships', 'character.skill.airplanes'],
       },
       {
         value: -1,
-        skills: [
-          'character.skill.knowledge.astronomy',
-          'character.skill.knowledge.biology-and-medicine',
-          'character.skill.knowledge.chemistry',
-        ],
+        skills: ['character.skill.astronomy', 'character.skill.biology-and-medicine', 'character.skill.chemistry'],
       },
     ],
   },
