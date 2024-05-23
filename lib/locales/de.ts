3ed5b35518f49d694511be8649a9aa8e45971812
spuxx1701 <accounts@spuxx.dev>
2024-05-23 17:14:59 +0200

    chore: Provide a couple of skill locales

--- lib/locales/de.ts
+++ lib/locales/de.ts
@@ -1,7 +1,8 @@
 import characterOrigin from '@/game-objects/character/origin/locales/de.yaml';
 import characterPreset from '@/game-objects/character/preset/locales/de.yaml';
 import primaryAttribute from '@/game-objects/character/primary-attribute/locales/de.yaml';
-import secondaryAttribute from '@/game-objects/character/secondary-attribute/locales/de.yaml';
+import secondaryAttribute from '@/game-objects/character/skill/locales/de.yaml';
+import skill from '@/game-objects/character/secondary-attribute/locales/de.yaml';
 import trait from '@/game-objects/character/trait/locales/de.yaml';
 import { deepMerge } from '@spuxx/browser-utils';
 
@@ -10,5 +11,6 @@ export const de: Record<string, unknown> = deepMerge(
   characterPreset,
   primaryAttribute,
   secondaryAttribute,
+  skill,
   trait,
 );
