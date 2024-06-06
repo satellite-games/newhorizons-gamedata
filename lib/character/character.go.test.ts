e0f200c25bc1c45f888a6e417f3592e7a8734b09
spuxx1701 <accounts@spuxx.dev>
2024-06-06 23:01:05 +0200

    test: Improve `Character.initialize` test

--- lib/character/character.go.test.ts
+++ lib/character/character.go.test.ts
@@ -1,10 +1,13 @@
 import { describe, expect, it } from 'vitest';
 import { Character } from '.';
 import { constants } from '@/constants';
+import { SecondaryAttribute, type PrimaryAttribute } from '@/main';
 
 describe('initialize', () => {
   it('should initialize with default name', async () => {
     const character = Character.initialize();
     expect(character.general.name).toBe(constants.CHARACTER_DEFAULT_NAME);
+    expect(character.getChildren<Character, PrimaryAttribute>('character.primary-attribute').length).toBe(8);
+    expect(character.getChildren<Character, SecondaryAttribute>('character.secondary-attribute').length).toBe(6);
   });
 });
